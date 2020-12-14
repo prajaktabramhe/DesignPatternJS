7b2007699a5b6adc2c30017e79fa624da265584d
prajaktabramhe <prajakta14bramhe@gmail.com>
2020-12-14 09:57:03 +0530

    [Prajakta]: Refactor Main file

--- factoryPattern/FactoryPatternMain.js
+++ factoryPattern/FactoryPatternMain.js
@@ -1,15 +1,6 @@
 
 let studentData = require('./FactoryPatternBusinessLogic')
 
-const student = []
 
-student.push(studentData.create("Prajakta", 1));
-student.push(studentData.create("Ekta", 2));
-student.push(studentData.create("Shreya", 3));
-
-function print() {
-    console.log("Student name " + this.name + " and knowledge about " + this.type);
-}
-student.forEach(stu => {
-    print.call(stu)
-})
+studentData.create("Python", 1 ,"Guido van Rossum");
+studentData.create("Java", 2, "James Goshling");
